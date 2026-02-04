b120bf3e3a44c96ace6241f08bfaf5d309647a79
Mitchell Sayre <sayremitch@gmail.com>
2026-02-03 19:51:11 -0500

    Switch to yarn and figure out how to import sherpa-onnx wasm commonjs bridge code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export function hello(name: string): string {
-  return `Hello, ${name}!`;
+// @ts-ignore
+import sherpaOnnx from "../wasm/index.cjs";
+
+export function greet(name: string): string {
+  const version = sherpaOnnx.version
+  return `Version, ${version}`;
 }
+
+console.log(greet("foo"))
